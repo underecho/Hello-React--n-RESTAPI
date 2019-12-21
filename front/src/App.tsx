5bd2092980c8d6dce0b9ade336dbed70172de51c
under <poke2000@live.jp>
2019-12-22 02:19:00 +0900

    Day3 - form test
    http://webdesign-dackel.com/2015/06/03/react-list-transition/

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,23 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type cardProps = {
+  //onSubmit: Function
+}
+
+type cardState = {
+  title: string
+  contents: string
+}
+
+class CardCtl extends React.Component<cardProps, cardState> {
+  constructor(props: cardProps){
+    super(props);
+    this.state = {title: "", contents: ""};
+  }
+
+  onChangeField(e: React.ChangeEvent){
+    // this.setState({ title: e.target.})
+  }
+
+  handleSubmit(e: React.FormEvent){
+    e.preventDefault()
+    let title = this.state.title.trim()
+
+  }
+
+  render(){
+    return (
+      <div className="cardCtl">
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text"
+            className="card-ctl-input"
+            placeholder="Card title"
+            value={this.state.title}
+            onChange={this.onChangeField}/>
+          <textarea
+            className="card-ctl-input"
+            placeholder="Card contents."
+            value={this.state.contents}
+            onChange={this.onChangeField}>
+          </textarea>
+          <button className="card-ctl-submit">Add!</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const flexItem: React.FC = () => {
+  return (
+    <div className="flex-item">
+      
+    </div>
+  );
+}
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React transition sample</h1>
+      <CardCtl />
     </div>
   );
 }
